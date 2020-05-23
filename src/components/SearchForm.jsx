c847ae3f9d5b35416753ceeb17f3fab841e6a4ac
joaocruz19 <joaovictorcruz82@gmail.com>
2020-05-23 13:32:50 -0300

    Added loading animation

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import SearchService from '../services/SearchService';
 import './SearchForm.css';
 import MovieComponent from './MovieComponent';
+import Loading from './Loading';
 
 class SearchForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { query: null, result: null };
+        this.state = { query: null, result: null, isLoading: false };
         this.SearchService = new SearchService();
         this.updateQuery = this.updateQuery.bind(this);
         this.searchMovie = this.searchMovie.bind(this);
@@ -20,42 +21,52 @@ class SearchForm extends Component {
 
     searchMovie(e) {
         e.preventDefault();
+        this.setState({ isLoading: true, result: null });
         const movieTitle = this.state.query;
         this.SearchService.searchMovie(movieTitle)
-            .then(res => this.setState({ result: res.data }));
+            .then(res => this.setState({ result: res.data }))
+            .then(() => this.setState({ isLoading: false }));
     }
 
     renderMovies() {
-        const { result } = this.state;
+        const { result, query } = this.state;
         if (result.success === false) {
             return (
                 <div className="alert alert-danger" role="alert">
                     {result.status_message}
                 </div>
-          );
+            );
         }
 
         const { results } = result;
-        return results.map(result => 
-            <MovieComponent movieInfo={result} key={this.key++} />
-        );
+        return (
+            <>
+                <p className="align-self-start">
+                    Showing {results.length} results for <strong>"{query}"</strong>
+                </p>
+                <div className="d-flex justify-content-around flex-wrap">
+                    {
+                        results.map(result =>
+                            <MovieComponent movieInfo={result} key={this.key++} />
+                        )
+                    }
+                </div>
+            </>
+        )
     }
 
     render() {
+        const { result, isLoading } = this.state;
         return (
             <div className="d-flex flex-column justify-content-start align-items-center">
                 <h2 className="mb-5">Search for a movie using this amazing React App!!</h2>
                 <form className="form-group d-flex" onSubmit={this.searchMovie}>
                     <input type="text" placeholder="Search a Movie..."
                         onChange={this.updateQuery} className="mx-2 form-control input-search shadow-none" />
-                    <div className="submit-wrapper">
-                        <input type="submit" value="" className="btn btn-search btn-danger shadow-none"/>
-                        <i className="fa fa-search"></i>
-                    </div>
+                    <input type="submit" value="&#xf002;" className="btn btn-search btn-danger shadow-none" />
                 </form>
-                <div className="d-flex justify-content-around flex-wrap">
-                    {this.state.result && this.renderMovies()}
-                </div>
+                {result && this.renderMovies()}
+                {isLoading && <Loading />}
             </div>
         )
     }
