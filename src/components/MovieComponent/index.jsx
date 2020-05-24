cf40d7044c41305ae371465cf4720ea2a5d24837
joaocruz19 <joaovictorcruz82@gmail.com>
2020-05-24 00:48:57 -0300

    added movies rate

--- src/components/MovieComponent/index.jsx
+++ src/components/MovieComponent/index.jsx
@@ -9,6 +9,27 @@ function MovieComponent(props) {
     const posterPath = movieInfo.poster_path &&`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`;
     const date = new Date(movieInfo.release_date);
 
+    function generateScore(score, maxScore) {
+        let newScore = score;
+        const starsClasses = [];
+        for (let i = 1; i <= maxScore; i++) {
+          if (newScore >= 1) starsClasses.push("star");
+          else if (newScore > 0) starsClasses.push("star-half");
+          else starsClasses.push("star star-off");
+          newScore--;
+        }
+        return (
+          <span className="stars-score">
+            <span>{score}</span>
+            {
+                starsClasses.map(starClass =>
+                    <i className={`fa fa-${starClass}`}></i>
+                )
+            }
+          </span>
+        );
+      }
+
     return (
         <>
             <div className="movie-container d-flex justify-content-around align-items-center m-3 py-2 px-3">
@@ -18,6 +39,9 @@ function MovieComponent(props) {
                 <div className="movie-info d-flex flex-column justify-content-around">
                 <h3>{movieInfo.title}</h3>
                 <small>Lauched: {date.toLocaleDateString()}</small>
+                <div>
+                    <span>Rate: {generateScore(movieInfo.vote_average, 10)}</span>
+                </div>
                 <p>{movieInfo.overview}</p>
                 <button className="btn btn-danger btn-more" onClick={() => setShowModal(true)}>More</button>
                 </div>
